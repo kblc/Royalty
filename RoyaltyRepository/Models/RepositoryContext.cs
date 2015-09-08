549639808b65453c857fa92ddc9a1fc2b1338027
stepan.sychev@gmail.com <admin3oo3>
2015-09-08 07:55:48 +0600

    Small changes in RepositoryContext

--- RoyaltyRepository/Models/RepositoryContext.cs
+++ RoyaltyRepository/Models/RepositoryContext.cs
@@ -11,35 +11,32 @@ namespace RoyaltyRepository.Models
 {
     public partial class RepositoryContext : DbContext
     {
+        #region Get Entity connection strings
+        private static string GetEntityConnectionString(string connectionString, string connectionProviderName)
+        {
+            return new System.Data.Entity.Core.EntityClient.EntityConnectionStringBuilder()
+            {
+                Provider = connectionProviderName,
+                ProviderConnectionString = connectionProviderName,
+            }.ToString();
+        }
+        private static string GetEntityConnectionString(string connectionStringName)
+        {
+            return new System.Data.Entity.Core.EntityClient.EntityConnectionStringBuilder()
+            {
+                Name = connectionStringName,
+            }.ToString();
+        }
+        #endregion
+
         static RepositoryContext()
         {
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<RepositoryContext, Migrations.Configuration>());
         }
 
-        public RepositoryContext() : 
-            base()
-        {
-        }
-
-        public RepositoryContext(string connectionStringName) :
-            base(string.Format("name={0}", connectionStringName))
-        {
-            //Database.Connection.ConnectionString = string.Format("name={0}", connectionStringName);
-        }
-
-        public RepositoryContext(string connectionString, string connectionProviderName)
-            : base(new System.Data.Entity.Core.EntityClient.EntityConnectionStringBuilder()
-            {
-                Provider = connectionProviderName,
-                ProviderConnectionString = connectionProviderName
-            }.ToString())
-        {
-            Database.Connection.ConnectionString = new System.Data.Entity.Core.EntityClient.EntityConnectionStringBuilder()
-            {
-                Provider = connectionProviderName,
-                ProviderConnectionString = connectionProviderName
-            }.ToString();
-        }
+        public RepositoryContext() : base() { }
+        public RepositoryContext(string connectionStringName) : base(GetEntityConnectionString(connectionStringName)) { }
+        public RepositoryContext(string connectionString, string connectionProviderName) : base(GetEntityConnectionString(connectionString, connectionProviderName)) { }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
